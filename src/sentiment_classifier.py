1a66899491a68ffe106a99703a7567648fbf0b38
Mina Labib <mina.n.habib@gmail.com>
2023-11-13 20:33:29 +0000

    sentiment classifer

--- src/sentiment_classifier.py
+++ src/sentiment_classifier.py
@@ -4,15 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": ""},
-        {"role": "user", "content": ""}
-    ],
-    temperature=0.7,
-    max_tokens=150,
-)
+while True:
+    user_input = input("Enter a phrase and we'll tell you if it is happy or sad.\n")
+    if user_input == "exit" or user_input == "quit":
+        break
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are sentiment classification bot, pring out if the user is happy or sad"},
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
 
-response_message = response["choices"][0]["message"]
-print(response_message)
+    response_message = response["choices"][0]["message"]
+    print(response_message)
